2b41e95a2f2ccbab607a18091e2fb5ac99fa0b8d
pberkman <patriciaberkman@Patricias-MacBook-Pro.local>
2014-08-20 15:48:02 -0400

    revert to original

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 	//initiate ajax call on page load
-	var hashtag = "themoscos";
+	var hashtag = "parisadventure";
 	searchPics(hashtag);
 
 	//hover over icons
@@ -47,58 +47,65 @@ $(document).ready(function() {
 	}); //end submit
 
 }); //end ready	
-	
+
+
+
 	var searchPics = function(hashtag) {
-	
+		
+
 		var getPhotos = $.ajax ({
 			url:"https://api.instagram.com/v1/tags/" + hashtag + "/media/recent?client_id=920d83e2718741b885af9d6323a498b4",
 			dataType:"jsonp",
 			type:"GET"
 		})
 		.done(function(getPhotos) {
+
+			var imageCounter=0;
+
 			var results = '<div class="gallery-wrapper">';
-			var slideResults = '<div id="slides">';
+			//var slideResults = '<div id="slides">';
+
 			$.each(getPhotos.data, function(index, photo) {
+				
 				results += '<div class="imageWrapper">';
-				slideResults += '<div class="slideImageWrapper">';
-				results += '<img src="' + photo.images.thumbnail.url + '"/>';
-				slideResults += '<img src="' + photo.images.standard_resolution.url + '"/>';
+				//slideResults += '<a href="javascript:setSlide(' + imageCounter + ')"<div class="slideImageWrapper">';
+				results += '<img src="' + photo.images.standard_resolution.url + '"/>';
+				//slideResults += '<img src="' + photo.images.standard_resolution.url + '"/>';
 				
 				//if statement for if there is no caption for the pics
 				if (photo.caption === null) {
 					results += '<div class="caption"></div>';
-					slideResults += '<div class="caption"></div>';
+					//slideResults += '<div class="caption"></div>';
 				} else {
 					results += '<div class="caption">' + photo.caption.text + '</div>';
-					slideResults += '<div class="caption">' + photo.caption.text + '</div>';
+					//slideResults += '<div class="caption">' + photo.caption.text + '</div>';
 				}
 
 				results += '</div>';
-				slideResults += '</div>';
+				//slideResults += '</div></a>';
+				imageCounter++;
 			});
-				results += '</div>';
-				slideResults += '</div>';
+				results += '</div>';//end of .gallery-wrapper
+				//slideResults += '</div>';//end of #slides
+
 				$(".gallery").html(results);
-				$(".slides-wrapper").html(slideResults);
+				//$(".slides-wrapper").html(slideResults);
 		
 
 				//click on image to bring up slideshow
-				$(".imageWrapper").click(function() {
+				/*$(".imageWrapper").click(function() {
 					//show slideshow box
 					$(".slideshow-container").show();
+					
 				});
 				//click on the "X" to exit the slideshow
 				$(".fa-times").click(function() {
 					$(".slideshow-container").hide();
-				});
-
-					//initiate slider
-					var slides = $("#slides");
-					slides.cycle({
-						fx:'scrollHorz'
-					}).cycle("pause");
-
-					//set up buttons to change pics
+				});*/
+		
+					
+/*
+					set up buttons to change pics
 					$("#right").click(function() {
 						slides.cycle('next');
 					});
@@ -110,5 +117,11 @@ $(document).ready(function() {
 					$('.arrow').hover(function() {
 						$(this).toggleClass("arrow-background");
 					});
-		}); //end .done()		
+
+*/
+
+		}); //end .done()
+
 	}; //end searchPics
+
+
